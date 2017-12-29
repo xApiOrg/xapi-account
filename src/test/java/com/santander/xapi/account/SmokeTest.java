f97630397f823b1fc1d9002b281b272b97230cbe
burgazov <ferdinand.milenov@vassit.co.uk>
2017-12-30 01:46:29 +0200

    Spring Boot and Runner smoke test

---
+++ src/test/java/com/santander/xapi/account/SmokeTest.java
@@ -0,0 +1,24 @@
+package com.santander.xapi.account;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.santander.xapi.account.controller.AccountController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest{
+	
+	@Autowired
+	private AccountController accountController;
+	
+	@Test
+	public void contextLoads() {
+		assertThat(accountController).isNotNull();
+	}
+}
