c5b470b89991e77c68e601db9762c53f509e6327
burgazov <ferdinand.milenov@vassit.co.uk>
2018-01-04 16:25:51 +0000

    Updates

--- src/main/java/com/santander/xapi/account/XapiAccountApplication.java
+++ src/main/java/com/santander/xapi/account/XapiAccountApplication.java
@@ -16,3 +16,26 @@ public class XapiAccountApplication {
 		SpringApplication.run(XapiAccountApplication.class, args);
 	}
 }
+
+/*
+ * 
+1. Create the configuration server application 
+	- locally https://start.spring.io/
+	- remotely 
+		https://console.run.pivotal.io/organizations/bd04ca83-2a38-4837-87b3-3f6ce2dbbc07/spaces/33d61af8-dd1b-4445-82f1-1c40230d95d7/services
+		
+		
+2. Application properties
+	spring.application.name=xapi-account-config-server
+	server.port=8888
+	spring.cloud.config.server.git.uri=file://${user.home}/config-service-example-config
+									or
+	spring.cloud.config.server.git.uri=https://github.com/xApiOrg/config-repo/blob/master/xapi-account.yml
+	
+	https://docs.pivotal.io/spring-cloud-services/1-3/common/config-server/
+
+3. 
+
+ * 
+ * 
+*/
