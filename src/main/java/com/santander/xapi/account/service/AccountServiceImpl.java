faa80f6ea9c7f8880aa936d62cfa8469cf98eb27
burgazov <ferdinand.milenov@vassit.co.uk>
2018-01-10 12:47:34 +0000

    Circuit breaker Hystrix introduced

--- src/main/java/com/santander/xapi/account/service/AccountServiceImpl.java
+++ src/main/java/com/santander/xapi/account/service/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package com.santander.xapi.account.service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
@@ -20,18 +23,30 @@ public class AccountServiceImpl implements AccountService {
 
 	@SuppressWarnings("unchecked")
 	@Override
+	@HystrixCommand(fallbackMethod="getAllPayableAccountsFallback")
 	public Set<Object> getAllPayableAccounts(Long userId) {
 		String uri = accountConfig.getProtocol() + accountConfig.getDomain() + accountConfig.getPath();
 		Set<Object> response = restTemplate.getForObject( uri + userId, Set.class );
 		return response;
 	}
+	
+	@HystrixCommand
+	private Set<Object> getAllPayableAccountsFallback(Long userId) {
+		return new HashSet<>();
+	}	
 
 	@Override
+	@HystrixCommand(fallbackMethod="getUserAccountByIdFallback")
 	public Object getUserAccountById(Long userId, Long id) {
 		String uri = accountConfig.getProtocol() + accountConfig.getDomain() + accountConfig.getPath();
 		Object response = restTemplate.getForObject( uri + userId + "/" + id, Object.class );
 		return response;
 	}
+	
+	@HystrixCommand
+	private Object getUserAccountByIdFallback(Long userId, Long id) {
+		return new Object();
+	}	
 	
 	@Primary
 	@Bean(name="restTemplateBean")
